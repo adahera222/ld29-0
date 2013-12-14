d7622cbbefe795b98edf8c922f80b69cbc1a0963
Yusuke Shinyama <yusuke@shinyama.jp>
2013-12-14 21:51:04 +0900

    added catapult sound.

---
+++ getone/Assets/Scripts/CatapultScript.cs
@@ -0,0 +1,18 @@
+﻿// -*- tab-width: 4 -*-
+using UnityEngine;
+using System.Collections;
+
+public class CatapultScript : MonoBehaviour {
+
+	void Start () {
+	
+	}
+	
+    void OnCollisionEnter2D(Collision2D coll) {
+        switch (coll.gameObject.tag) {
+        case "Ball":
+            audio.Play();
+            break;
+        }
+    }
+}
