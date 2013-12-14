aa61e7c58096399b0d76d43dc69fc2dd0c3d6b64
Yusuke Shinyama <yusuke@shinyama.jp>
2013-12-14 17:51:17 +0900

    added guide arrow.

---
+++ getone/Assets/Scripts/GuideArrowScript.cs
@@ -0,0 +1,21 @@
+﻿// -*- tab-width: 4 -*-
+using UnityEngine;
+using System.Collections;
+
+public class GuideArrowScript : MonoBehaviour {
+
+    private Transform goal;
+
+	void Start () {
+        GameObject obj = GameObject.FindWithTag("Goal");
+        goal = obj.transform;
+	}
+	
+	void Update () {
+        // Direct the goal.
+        Vector2 pos0 = new Vector3(transform.position.x, transform.position.y, 0);
+        Vector2 pos1 = new Vector3(goal.position.x, goal.position.y, 0);
+        float angle = Vector2.Angle(pos1-pos0, Vector2.right);
+        transform.rotation = Quaternion.AngleAxis(-angle, Vector3.forward);
+	}
+}
