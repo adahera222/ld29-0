2f44681cd9e98cfec333369d3269482702ee8ccb
Yusuke Shinyama <yusuke@shinyama.jp>
2013-12-14 18:59:20 +0900

    added trigger and falling floor

---
+++ getone/Assets/Scripts/FallingFloorScript.cs
@@ -0,0 +1,25 @@
+﻿// -*- tab-width: 4 -*-
+using UnityEngine;
+using System.Collections;
+
+public class FallingFloorScript : MonoBehaviour {
+
+    public Transform trapdoor = null;
+
+    private float _gravityScale;
+
+	void Start () {
+        _gravityScale = rigidbody2D.gravityScale;
+        rigidbody2D.gravityScale = 0f;
+        if (trapdoor != null) {
+            TrapDoorScript script = trapdoor.gameObject.GetComponent<TrapDoorScript>();
+            if (script != null) {
+                script.activated += OnTrapDoorActivated;
+            }
+        }
+	}
+	
+    void OnTrapDoorActivated(object sender, System.EventArgs args) {
+        rigidbody2D.gravityScale = _gravityScale;
+	}
+}
---
+++ getone/Assets/Scripts/TrapDoorScript.cs
@@ -0,0 +1,27 @@
+﻿// -*- tab-width: 4 -*-
+using UnityEngine;
+using System.Collections;
+
+public class TrapDoorScript : MonoBehaviour {
+
+    public event System.EventHandler activated;
+
+    private bool _on = false;
+
+	void Start () {
+	}
+	
+    void OnCollisionEnter2D(Collision2D coll) {
+        switch (coll.gameObject.tag) {
+        case "Ball":
+            if (!_on) {
+                _on = true;
+                if (activated != null) {
+                    activated(this, new System.EventArgs());
+                }
+                audio.Play();
+            }
+            break;
+        }
+    }
+}
