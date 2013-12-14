dc5456593343085f3ba4e04ec147ed3cb6f33481
Yusuke Shinyama <yusuke@shinyama.jp>
2013-12-14 12:08:39 +0900

    derp.

---
+++ getone/Assets/Scripts/BallScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+            Vector2 d = new Vector2(100, 400);
+            rigidbody2D.AddForce(d);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
